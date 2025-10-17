81ab829a5cdd118b0400c462f701ff789fa41fd3
Shashi <shashidharakv@examroom.ai>
2025-10-17 13:12:25 +0530

    buid error fix

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -6,8 +6,8 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import { register } from '@/routes/index.ts';
-import { request } from '@/routes/password/index.ts';
+import { register } from '@/routes';
+import { request } from '@/routes/password';
 import { Form, Head } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 
